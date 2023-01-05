d11048038c9f469ed2bd5206e951e114bba8b1d8
gabry <g.benvenuti58@gmail.com>
2023-01-05 12:30:42 +0100

    ancora

--- nuovo.js
+++ nuovo.js
@@ -1,1 +1,10 @@
 console.log('Hello branch');
+
+////////////////////
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", togli);
+
+function togli() {
+    document.querySelector(".branch-class").innerHTML = "bravo";
+}
